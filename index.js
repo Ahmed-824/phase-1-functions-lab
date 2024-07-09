9e5b5348d5c4a317a04aa1d485464dc39ab35109
Ahmedaliamour <abdalaamour69@gmail.com>
2024-07-09 21:56:53 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+        return blocks - 42;
+    } else {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264; 
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    let blocksTravelled = Math.abs(startBlock - endBlock);
+    return blocksTravelled * 264; 
+}
+function calculatesFarePrice(startBlock, endBlock) {
+    let feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
+    
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+        return 25;
+    } else if(feetTravelled > 2500){
+        return 'cannot travel that far';
+    }
+}
